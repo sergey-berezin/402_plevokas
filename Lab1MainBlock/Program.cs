6fd6ba97ff77030d3636842d5feb8ed3a2ebb946
Yanis Plevokas <58010275+YanisPlevokas@users.noreply.github.com>
2021-10-13 23:36:38 +0300

    Update Program.cs

--- Lab1MainBlock/Program.cs
+++ Lab1MainBlock/Program.cs
@@ -1,29 +1,61 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using LabMainBlock;
+using YOLOv4MLNet;
+using System.Threading;
+using System.Threading.Tasks;
 using YOLOv4MLNet.DataStructures;
+using System.Collections.Concurrent;
+using System.Linq;
 
 namespace Lab1MainBlock
 {
     class Program
     {
-        static void Main(string[] args)
+        static public ConcurrentQueue<Tuple<string, IReadOnlyList<YoloV4Result>>> yoloResults = new ConcurrentQueue<Tuple<string, IReadOnlyList<YoloV4Result>>>();
+        static public List<string> labelsList = new List<string>();
+        static async Task Main(string[] args)
         {
-            var new_var = Program1.MainBlock(@"Assets\Images");
-            Tuple<string, IReadOnlyList<YoloV4Result>> res;
-            while (!Program1.isEnded)
+
+            var cst = new CancellationTokenSource();
+            var ct = cst.Token;
+
+            Tuple<string, IReadOnlyList<YoloV4Result>> result;
+            string path_for_prog = @"Assets\Images";
+
+            var cancellationTask = Task.Run(() =>
             {
-                while (Program1.yoloResults.TryDequeue(out res))
-                {
-                    Console.WriteLine(res.Item1);
-                    foreach (var obj in res.Item2)
-                    {
-                        Console.WriteLine(obj.Label + " " + obj.BBox[0] + " " + obj.BBox[1] + " " + obj.BBox[2] + " " + obj.BBox[3]);
-                    }
-                    Console.WriteLine(" ");
-                }
+                string cancel = Console.ReadLine();
+                if (cancel == "s" || cancel == "S")
+                    cst.Cancel();
             }
-            Console.WriteLine("\nDear user, work finished, press enter to continue");
+            );
+            var task1 = ImageProcessing.SuperImageProphet(path_for_prog, yoloResults, ct);
+            var task2 = Task.Run(() =>
+            {
+                while (true)
+                {
+                    while (yoloResults.TryDequeue(out result))
+                    {
+                        // печать
+                        var file_name = result.Item1;
+                        var file_info = result.Item2;
+
+                        foreach (var object_new in file_info)
+                        {
+                            labelsList.Add(object_new.Label);
+                        }
+                        var query = labelsList.GroupBy(a => a).Select(x => new { key = x.Key, val = x.Count()});
+                        Console.WriteLine("{0} - file name",file_name);
+                        foreach (var result in query)
+                        {
+                            Console.WriteLine("{0} - object_name, {1} - count", result.key, result.val);
+                        }
+                    }
+                }
+
+            });
+            await Task.WhenAll(task1);
         }
     }
 }
